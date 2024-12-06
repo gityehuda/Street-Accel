e3c4c1d64cb4494067567d8fc50cbebc8f1cbc86
yehuda24 <yehuda.ekglecia@skkkjakarta.sch.id>
2024-12-06 12:05:37 +0700

    Update 2024.12.6

--- Assets/Assets/Script/EnemyMovement.cs
+++ Assets/Assets/Script/EnemyMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public float speed = 1f;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
